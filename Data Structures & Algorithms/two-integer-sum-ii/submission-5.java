e3395466dfe0ced9d573167561e4199823646c42
Pavel Maisiuk-Dranko <dev@palgogo.com>
2026-09-16 15:49:51 +0200

    Add: two-integer-sum-ii - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-5.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        for(int i = 0; i < numbers.length; i++){
+            int left = i + 1;
+            int right = numbers.length - 1;
+            int tmp = target - numbers[i];
+            while(left <= right){
+                int mid = (right - left + 1)/2;
+                
+                if(numbers[mid] == tmp){
+                    return new int[] {i +1, mid + 1};
+                }
+                if(numbers[mid] < tmp){
+                    left = mid + 1;
+                }
+                if (numbers[mid] > tmp){
+                    right = mid - 1;
+                }
+                
+            }
+        }
+
+        return new int[0];
+    }
+}
